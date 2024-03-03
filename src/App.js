4a1b1b7e19c003d09f8b38bc461bf29914e99867
sakshamdubeyyy <109180511+sakshamdubeyyy@users.noreply.github.com>
2024-03-03 18:52:05 +0530

    complete

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
+import { useReducer, useState } from 'react';
 import './App.css';
+import Todo from './Todo';
+
+export const ACTIONS = {
+  ADD_TODO: 'add-todo',
+  TOGGLE_TODO: 'toggle-todo',
+  DELETE_TODO: 'delete-todo'
+}
+
+const reducer = (todos, action) => {
+  switch (action.type){
+    case ACTIONS.ADD_TODO:
+      return [ ...todos, newTodo(action.payload.task)]
+    case ACTIONS.TOGGLE_TODO:
+      return todos.map(todo => {
+        if(todo.id === action.payload.id){
+          return {...todo, complete: !todo.complete}
+        }
+        return todo;
+      })
+    case ACTIONS.DELETE_TODO:
+      return todos.filter(todo => todo.id !== action.payload.id);
+    default:
+      return todos
+  }
+}
+
+const newTodo = (task) => {
+  return { id: Date.now(), task: task, complete: false }
+}
 
 function App() {
+  const [todos, dispatch] = useReducer(reducer, []);
+  const [task, setTask] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch({type: ACTIONS.ADD_TODO, payload: {task: task}});
+    setTask('');
+  } 
+
+  console.log(todos);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleSubmit}>
+        <input value={task} onChange={e => setTask(e.target.value)}/> 
+      </form>
+      {todos.map(todo => (
+        <Todo key= {todo.id} todo= {todo} dispatch={dispatch} />
+      ))}
     </div>
   );
 }
